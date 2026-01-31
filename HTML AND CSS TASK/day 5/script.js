6b28e4373b21fb5fd4f8fccb5ada18662fd33e9f
SandhiyaRamamoorthy25 <ramamoorthysandhiya2@gmail.com>
2026-01-31 10:14:10 +0530

    Add files via upload

---
+++ HTML AND CSS TASK/day 5/script.js
@@ -0,0 +1,107 @@
+ {
+   var price = 300
+    var product='tws'
+    var tax=10
+}
+console.log(price+tax)
+console.log(product)
+
+{
+    var fruitName ="Apple"
+    var count = 2
+    var price = 300
+}
+console.log(fruitName)
+console.log(count*price)
+
+function redmi()
+{
+console.log("this is redmi mobile")
+}
+redmi()
+function add()
+{
+    var a=1
+    var b=1
+    console.log(a+b)
+}
+add()
+
+var favActor = " kamal"
+var favPlayer = "dhoni"
+var favMovie = "sivam"
+function fav()
+{
+    console.log('Favourite Actor : '+favActor)
+    console.log('Favourite Player  : '+favPlayer)
+    console.log( 'Favourite Movie: '+favMovie)
+}
+fav()
+
+function area(l,b){
+    console.log("area is :"+l*b)
+    console.log("length is :"+l+b)
+}
+area(10,20)
+ 
+var work = false
+if(work){
+    console.log("great job!")
+}
+else{
+     console.log("finish ur work")
+}
+
+var cookies = true
+if(cookies){
+    console.log("would you like u a cookies")
+}
+else{
+    console.log("time to bake more cookies")
+}
+
+var season = "spring"
+if(season == "spring"){
+    console.log("enjoy the blooming flowers")
+}
+else if(season == "summer"){
+    console.log("have fun in summer")
+}
+else if(season == "autumn"){
+    console.log("admire colourful leaves")
+}
+else if(season == "winter"){
+    console.log("bundle up and stay warm")
+}
+else if("not a season"){
+    console.log("enter a season")
+}
+
+ var score = 60
+ if(score<=50){
+    console.log("you need to improve")
+ } 
+ else if(score>50 && score<=70){
+    console.log("good job")
+ }
+ else if(score>70){
+    console.log("excellent")
+ }
+
+const num = 5;
+if(num%2==0){
+    console.log(num+"is even")
+}
+else{
+    console.log(num+"is odd")
+}
+const vowel ="aeiouAEIOU";
+const char="d"
+if(vowel.includes(char)){
+    console.log("its a vowel")
+}
+else{
+
+    console.log("its a conosont")
+}
+
